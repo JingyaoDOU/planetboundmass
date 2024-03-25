5c97c9f4b29c1be48bcc8c61110c7b0ecb192ce7
JingyaoDOU <jingyaodou.uk@gmail.com>
2024-03-25 11:31:01 +0000

    add functions

--- planetboundmass/SWIFT_funcs.py
+++ planetboundmass/SWIFT_funcs.py
@@ -739,3 +739,483 @@ def print_impact_info(loc_tar, loc_imp, Xves=1.0, verbose=1):
         print("%.3f times the mutual escape velocity = %f m/s" % (times, v_esc * times))
 
     return v_esc, gamma
+
+
+def loadsw_to_woma(snapshot, unit="mks", npR=1000):
+    # Load
+    data = sw.load(snapshot)
+    if unit == "mks":
+
+        box_mid = 0.5 * data.metadata.boxsize[0].to(unyt.m)
+        data.gas.coordinates.convert_to_mks()
+        pos = np.array(data.gas.coordinates - box_mid)
+        data.gas.velocities.convert_to_mks()
+        vel = np.array(data.gas.velocities)
+        data.gas.smoothing_lengths.convert_to_mks()
+        h = np.array(data.gas.smoothing_lengths)
+        data.gas.masses.convert_to_mks()
+        m = np.array(data.gas.masses)
+        data.gas.densities.convert_to_mks()
+        rho = np.array(data.gas.densities)
+        data.gas.pressures.convert_to_mks()
+        p = np.array(data.gas.pressures)
+        data.gas.internal_energies.convert_to_mks()
+        u = np.array(data.gas.internal_energies)
+        matid = np.array(data.gas.material_ids)
+        # pid     = np.array(data.gas.particle_ids)
+
+    elif unit == "cgs":
+
+        box_mid = 0.5 * data.metadata.boxsize[0].to(unyt.cm)
+        data.gas.coordinates.convert_to_cgs()
+        pos = np.array(data.gas.coordinates - box_mid)
+        data.gas.velocities.convert_to_cgs()
+        vel = np.array(data.gas.velocities)
+        data.gas.smoothing_lengths.convert_to_cgs()
+        h = np.array(data.gas.smoothing_lengths)
+        data.gas.masses.convert_to_cgs()
+        m = np.array(data.gas.masses)
+        data.gas.densities.convert_to_cgs()
+        rho = np.array(data.gas.densities)
+        data.gas.pressures.convert_to_cgs()
+        p = np.array(data.gas.pressures)
+        data.gas.internal_energies.convert_to_cgs()
+        u = np.array(data.gas.internal_energies)
+        matid = np.array(data.gas.material_ids)
+        # pid     = np.array(data.gas.particle_ids)
+    else:
+        raise TypeError("Wrong unit selection, please check!!")
+
+    pos_centerM = np.sum(pos * m[:, np.newaxis], axis=0) / np.sum(m)
+    vel_centerM = np.sum(vel * m[:, np.newaxis], axis=0) / np.sum(m)
+
+    pos -= pos_centerM
+    vel -= vel_centerM
+
+    pos_noatmo = pos[matid != 200]
+
+    xy = np.hypot(pos_noatmo[:, 0], pos_noatmo[:, 1])
+    r = np.hypot(xy, pos_noatmo[:, 2])
+    r = np.sort(r)
+    R = np.mean(r[-npR:])
+
+    return pos, vel, h, m, rho, p, u, matid, R
+
+
+# PLOT COLOR SHADED VALUES RHO-T TABLE
+lw = 1
+sfont = 15
+ds = (10, 10)
+
+
+def eos_plot(NewEOS, ax0, rho=None, T=None, P=None, point_color=False, lim=[2, 30]):
+    """
+    NewEOS: the name of the EOS table.
+    ax0: passed in axis
+    rho: densities array
+    T: temperatures array
+    P: Pressures array
+    Point_color: When point_color is turned on, the scatter points will have colors according to their pressure. This will mainly use to check if the
+        particles or planet pressure have the right value. Scatter points turn to be "transparant" if all the values are correct
+        and you will only see the background color.
+    """
+
+    transEOS_P = (np.log10(NewEOS.P) + 25) / 29.0
+    transDATA_P = (np.log10(P) + 25) / 29.0
+    _min = np.min([np.amin(transEOS_P), np.amin(transDATA_P)])
+    _max = np.max([np.amax(transEOS_P), np.amax(transDATA_P)])
+
+    fig = plt.figure(figsize=(25, 7))
+
+    im1 = ax0.pcolormesh(
+        NewEOS.rho,
+        NewEOS.T,
+        np.log10(NewEOS.P),
+        cmap=local_cmaps.parula,
+        edgecolors="face",
+    )
+    if not point_color:
+        ax0.scatter(rho, T, c="limegreen", s=3, linewidth=0.01)
+    else:
+        ax0.scatter(
+            rho,
+            T,
+            c=transDATA_P,
+            cmap=local_cmaps.parula,
+            vmin=_min,
+            vmax=_max,
+            edgecolors="face",
+            s=1,
+        )
+    ax0.plot(
+        NewEOS.vc.rl,
+        NewEOS.vc.T,
+        color="black",
+        linewidth=lw,
+        linestyle="--",
+        dashes=ds,
+    )
+    ax0.plot(
+        NewEOS.vc.rv,
+        NewEOS.vc.T,
+        color="black",
+        linewidth=lw,
+        linestyle="--",
+        dashes=ds,
+    )
+    ax0.plot(NewEOS.vc.rl[0], NewEOS.vc.T[0], "ko")
+    if NewEOS.mc.NT > 0:
+        ax0.plot(
+            NewEOS.mc.rl,
+            NewEOS.mc.T,
+            color="black",
+            linewidth=lw,
+            linestyle="--",
+            dashes=ds,
+        )
+    #    ax0.plot(NewEOS.mc.rs,NewEOS.mc.T,color="black",linewidth=lw,linestyle='--',dashes=ds)
+
+    ax0.set_ylim(0.0, 20000)
+    ax0.set_xlim(lim[0], lim[1])
+    ax0.set_xlabel("Density (g/cm$^3$)", size=sfont)
+    ax0.set_ylabel("Temperature (K)", size=sfont)
+    ax0.set_title("Colors=log$_{10}$(P (GPa))", size=sfont)
+    ax0.tick_params(labelsize=sfont)
+
+    divider0 = make_axes_locatable(ax0)
+    cax0 = divider0.append_axes("right", size="5%", pad=0.05)
+    cbar0 = fig.colorbar(im1, cax=cax0, orientation="vertical")
+    cbar0.set_label("Log$_{10}$(P) [GPa]", size=sfont)
+
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+    return ax0
+
+
+def eos_planet_plot(
+    planet=None,
+    loc=None,
+    coreEOS=None,
+    mantleEOS=None,
+    core_lim=[2, 30],
+    mantle_lim=[2, 30],
+):
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(15, 12))
+    if planet is not None:
+
+        ax1 = eos_plot(
+            coreEOS,
+            ax1,
+            planet.A1_rho[planet.A1_mat_id > 400] * 1e-3,
+            planet.A1_T[planet.A1_mat_id > 400],
+            planet.A1_P[planet.A1_mat_id > 400] * 1e-9,
+            lim=core_lim,
+        )
+        ax2 = eos_plot(
+            coreEOS,
+            ax2,
+            planet.A1_rho[planet.A1_mat_id > 400] * 1e-3,
+            planet.A1_T[planet.A1_mat_id > 400],
+            planet.A1_P[planet.A1_mat_id > 400] * 1e-9,
+            point_color=True,
+            lim=core_lim,
+        )
+
+        ax3 = eos_plot(
+            mantleEOS,
+            ax3,
+            planet.A1_rho[planet.A1_mat_id == 400] * 1e-3,
+            planet.A1_T[planet.A1_mat_id == 400],
+            planet.A1_P[planet.A1_mat_id == 400] * 1e-9,
+            lim=mantle_lim,
+        )
+        ax4 = eos_plot(
+            mantleEOS,
+            ax4,
+            planet.A1_rho[planet.A1_mat_id == 400] * 1e-3,
+            planet.A1_T[planet.A1_mat_id == 400],
+            planet.A1_P[planet.A1_mat_id == 400] * 1e-9,
+            point_color=True,
+            lim=mantle_lim,
+        )
+    else:
+
+        data = sw.load(loc)
+        data.gas.densities.convert_to_cgs()
+        rho_cgs = np.array(data.gas.densities)
+        data.gas.densities.convert_to_mks()
+        rho_mks = np.array(data.gas.densities)
+        data.gas.pressures.convert_to_mks()
+        p_mks = np.array(data.gas.pressures)
+        matid = np.array(data.gas.material_ids)
+        data.gas.internal_energies.convert_to_mks()
+        u_mks = np.array(data.gas.internal_energies)
+
+        T = woma.eos.eos.A1_T_u_rho(u_mks, rho_mks, matid)
+
+        ax1 = eos_plot(
+            coreEOS,
+            ax1,
+            rho_cgs[matid > 400],
+            T[matid > 400],
+            p_mks[matid > 400] * 1e-9,
+        )
+        ax2 = eos_plot(
+            coreEOS,
+            ax2,
+            rho_cgs[matid > 400],
+            T[matid > 400],
+            p_mks[matid > 400] * 1e-9,
+            point_color=True,
+        )
+
+        ax3 = eos_plot(
+            mantleEOS,
+            ax3,
+            rho_cgs[matid == 400],
+            T[matid == 400],
+            p_mks[matid == 400] * 1e-9,
+        )
+        ax4 = eos_plot(
+            mantleEOS,
+            ax4,
+            rho_cgs[matid == 400],
+            T[matid == 400],
+            p_mks[matid == 400] * 1e-9,
+            point_color=True,
+        )
+
+    plt.tight_layout()
+
+
+Ng = 1601j
+Ngz = 201j
+
+
+def sw_profile_plot(
+    file_loc,
+    ax_lim=2.0,
+    Entropy=True,
+    idealgas=False,
+    center=True,
+    colourplot=False,
+    Till=False,
+    compareloc=None,
+    planet=None,
+):
+    # Load
+    # woma.load_eos_tables()
+
+    data = sw.load(file_loc)
+    box_mid = 0.5 * data.metadata.boxsize[0]
+    pos = data.gas.coordinates - box_mid
+    pos = np.array(pos)
+    data.gas.densities.convert_to_cgs()
+    rho_cgs = np.array(data.gas.densities)
+    data.gas.densities.convert_to_mks()
+    rho_mks = np.array(data.gas.densities)
+    data.gas.pressures.convert_to_cgs()
+    p = np.array(data.gas.pressures)
+    data.gas.internal_energies.convert_to_mks()
+    u = np.array(data.gas.internal_energies)
+    matid = data.gas.material_ids
+    data.gas.pressures.convert_to_mks()
+    p_mks = np.array(data.gas.pressures)
+    matid = np.array(matid)
+    m = np.array(data.gas.masses)
+    colour = np.empty(len(matid), dtype=object)
+    for id_c, c in Di_id_colour.items():
+        colour[matid == id_c] = c
+
+    entropy = np.zeros_like(p)
+    T = np.zeros_like(p)
+
+    if compareloc is not None:
+        data2 = sw.load(compareloc)
+        box_mid = 0.5 * data2.metadata.boxsize[0]
+        pos2 = data2.gas.coordinates - box_mid
+        pos2 = np.array(pos2)
+        data2.gas.densities.convert_to_cgs()
+        rho_cgs2 = np.array(data2.gas.densities)
+        data2.gas.densities.convert_to_mks()
+        rho_mks2 = np.array(data2.gas.densities)
+        data2.gas.pressures.convert_to_cgs()
+        p2 = np.array(data2.gas.pressures)
+        data2.gas.internal_energies.convert_to_mks()
+        u2 = np.array(data2.gas.internal_energies)
+        matid2 = data2.gas.material_ids
+        data2.gas.pressures.convert_to_mks()
+        p_mks2 = np.array(data2.gas.pressures)
+        matid2 = np.array(matid2)
+        m2 = np.array(data2.gas.masses)
+
+        colour2 = np.empty(len(matid2), dtype=object)
+        for id_c, c in Di_id_colour.items():
+            colour2[matid2 == id_c] = c
+
+        if center:
+            pos_centerM = np.sum(pos2 * m2[:, np.newaxis], axis=0) / np.sum(m2)
+            pos2 -= pos_centerM
+        T2 = woma.eos.eos.A1_T_u_rho(u2, rho_mks2, matid2)
+        entropy2 = woma.eos.eos.A1_s_u_rho(u2, rho_mks2, matid2)
+
+        XY2 = np.hypot(pos2[:, 0], pos2[:, 1])
+        R2 = np.hypot(XY2, pos2[:, 2])
+
+    colour = np.empty(len(matid), dtype=object)
+    for id_c, c in Di_id_colour.items():
+        colour[matid == id_c] = c
+
+    entropy = np.zeros_like(p)
+    T = np.zeros_like(p)
+
+    if center:
+        # data.gas.masses.convert_to_mks()
+
+        pos_centerM = np.sum(pos * m[:, np.newaxis], axis=0) / np.sum(m)
+        # vel_centerM = np.sum(vel * m[:,np.newaxis], axis=0) / np.sum(m)
+
+        pos -= pos_centerM
+        # vel -= vel_centerM
+
+    if Entropy:
+        sel = (matid != 200) & (matid != 0)
+        entropy[sel] = woma.eos.eos.A1_s_u_rho(u[sel], rho_mks[sel], matid[sel])
+        # T       = woma.eos.eos.A1_T_u_rho(u,rho_mks,matid)
+
+    if idealgas:
+        T[matid == 0] = u[matid == 0] / 9093.98
+        T[matid != 0] = woma.eos.eos.A1_T_u_rho(
+            u[matid != 0], rho_mks[matid != 0], matid[matid != 0]
+        )
+    else:
+        T = woma.eos.eos.A1_T_u_rho(u, rho_mks, matid)
+
+    #     minT        = np.min(T)
+    #     maxT        = np.max(T)
+
+    # s=woma.eos.eos.A1_s_u_rho(u,rho,matid)
+
+    XY = np.hypot(pos[:, 0], pos[:, 1])
+    R = np.hypot(XY, pos[:, 2])
+    # if 0 in matid:
+    #     Ratmo=np.sort(R[matid==0])
+    #     thick=Ratmo[-1]-Ratmo[0]
+    # elif 200 in matid:
+    #     Ratmo=np.sort(R[matid==200])
+    #     thick=Ratmo[-1]-Ratmo[0]
+
+    print("Read %d particles!" % (len(R)))
+
+    # fig,ax=plt.subplots(figsize=(6,5))
+
+    #     X,Y= np.mgrid[-ax_lim:ax_lim:(Ng),-ax_lim:ax_lim:(Ng)]
+
+    #     Tis= scipy.interpolate.griddata((pos[:,0],pos[:,1]),T,(X,Y),method='linear',fill_value=1.e-18)
+    #     palette = copy(plt.get_cmap('hot'))
+    #     palette.set_under('black', 1.0)  # 1.0 represents not transparent
+    #     levels = np.arange(minT-50, maxT+50, 100)
+    #     norms = colors.BoundaryNorm(levels, ncolors=palette.N)
+    # im=ax.imshow(Tis.T,origin='lower',extent=[xmin,xmax,ymin,ymax],norm=norms,cmap=palette,aspect='auto')
+    # cbar = fig.colorbar(im, extend='min', shrink=0.9, ax=ax)
+    # plt.clf()
+    # return Tis,norms,palette,T, entropy, u, rho_cgs, p_mks, R
+
+    fig, axs = plt.subplots(3, 2, figsize=(12, 15))
+    axs = axs.ravel()
+
+    # particles_R=np.sqrt(particles.A1_x**2+particles.A1_y**2+particles.A1_z**2)
+
+    # with sns.axes_style("white"):
+    # im_woma=axs[0].imshow(Tis_woma.T,origin='lower',extent=[xmin,xmax,ymin,ymax],norm=norms,cmap=palette,aspect='auto')
+    # cbar_woma = fig.colorbar(im_woma, extend='min', shrink=0.9, ax=axs[0])
+    # axs[0].set_title(r'O.89998 $M_\oplus$ 0.913x$10^7$',fontsize=22)
+    # axs[0].set_facecolor('black')
+    # axs[0].tick_params(axis="x", direction='in')
+    # axs[0].tick_params(axis="y", direction='in')
+
+    if colourplot:
+        axs[1].scatter(R, T, s=1, c=colour)
+    else:
+        axs[1].scatter(R, T, s=1)
+        if compareloc is not None:
+            axs[1].scatter(R2, T2, s=1, label="compare group")
+            axs[1].legend()
+    if planet is not None:
+        axs[1].scatter(planet.A1_r / R_earth, planet.A1_T, s=1, alpha=0.6)
+    axs[1].tick_params(axis="x", direction="in")
+    axs[1].tick_params(axis="y", direction="in")
+    axs[1].set_title("Temperature profile", fontsize=22)
+    axs[1].set_xlabel(r"$R\oplus$")
+    axs[1].set_ylabel(r"T K")
+    axs[1].set_yscale("log")
+
+    if colourplot:
+        axs[2].scatter(R, rho_cgs, s=1, c=colour)
+    else:
+        axs[2].scatter(R, rho_cgs, s=1)
+        if compareloc is not None:
+            axs[2].scatter(R2, rho_cgs2, s=1, label="compare group")
+            axs[2].legend()
+    if planet is not None:
+        axs[2].scatter(planet.A1_r / R_earth, planet.A1_rho * 1e-3, s=1)
+    axs[2].set_title(r"Density $\rho$ profiles", fontsize=22)
+    axs[2].set_xlabel(r"$R\oplus$")
+    axs[2].set_ylabel(r"$\rho$ $g/cm^{3}$")
+    axs[2].set_yscale("log")
+    axs[2].tick_params(axis="x", direction="in")
+    axs[2].tick_params(axis="y", direction="in")
+
+    if colourplot:
+        axs[3].scatter(R, p_mks / 1e9, s=1, c=colour)
+    else:
+        axs[3].scatter(R, p_mks / 1e9, s=1)
+        if compareloc is not None:
+            axs[3].scatter(R2, p_mks2 / 1e9, s=1, label="compare group")
+            axs[3].legend()
+    if planet is not None:
+        axs[3].scatter(planet.A1_r / R_earth, planet.A1_P * 1e-9, s=1)
+    axs[3].set_title("Pressure profile", fontsize=22)
+    axs[3].set_xlabel(r"$R\oplus$")
+    axs[3].set_ylabel("Gpa")
+    axs[3].set_yscale("log")
+    axs[3].tick_params(axis="x", direction="in")
+    axs[3].tick_params(axis="y", direction="in")
+
+    if not Till:
+        if colourplot:
+            axs[4].scatter(R, entropy, s=1, c=colour)
+        else:
+            axs[4].scatter(R, entropy, s=1)
+            if compareloc is not None:
+                axs[4].scatter(R2, entropy2, s=1, label="compare group")
+                axs[4].legend()
+        axs[4].set_title("Entropy profile", fontsize=22)
+        axs[4].set_xlabel(r"$R\oplus$")
+        axs[4].set_ylabel("J/kg/K")
+        axs[4].tick_params(axis="x", direction="in")
+        axs[4].tick_params(axis="y", direction="in")
+
+    if colourplot:
+        axs[5].scatter(R, u, s=1, c=colour)
+    else:
+        axs[5].scatter(R, u, s=1)
+        if compareloc is not None:
+            axs[5].scatter(R2, u2, s=1, label="compare group")
+            axs[5].legend()
+    if planet is not None:
+        axs[5].scatter(planet.A1_r / R_earth, planet.A1_u, s=1)
+    axs[5].set_title("Internal energy profile", fontsize=22)
+    axs[5].set_xlabel(r"$R\oplus$")
+    axs[5].set_ylabel("J/kg")
+    axs[5].set_yscale("log")
+    axs[5].tick_params(axis="x", direction="in")
+    axs[5].tick_params(axis="y", direction="in")
+
+    plt.tight_layout()
+    plt.show()
+    plt.cla()
+    plt.clf()
+    plt.close()
