541a45bfdd5325ccfe9c3d24fe40ae5532d698dd
JingyaoDOU <jingyaodou.uk@gmail.com>
2024-05-31 20:15:10 +0100

    tst

--- planetboundmass/__init__.py
+++ planetboundmass/__init__.py
@@ -1,3 +1,5 @@
 # from planetboundmass import boundmass
 # from planetboundmass.boundmass import Bound, Snap
 # from .boundmass import Bound, Snap
+from planetboundmass.boundmass import Bound, Snap
+import planetboundmass
