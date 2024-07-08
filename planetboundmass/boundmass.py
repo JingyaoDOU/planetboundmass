3fff3aced14b541ea581f04200a45c9e39cac172
JingyaoDOU <jingyaodou.uk@gmail.com>
2024-07-08 18:34:32 +0100

    fix attribute name error

--- planetboundmass/boundmass.py
+++ planetboundmass/boundmass.py
@@ -32,7 +32,7 @@ class Bound:
         maxit=1000,
         max_bad_seeds=1000,
         verbose=1,
-        hitdirection="pX",
+        hit_direction="pX",
     ):
         if not filename.endswith("hdf5"):
             raise TypeError("Wrong filename, please check the file extension")
