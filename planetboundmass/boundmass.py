dd1b47c0a916c65d10b8bd318128a96ca756c575
JingyaoDOU <jingyaodou.uk@gmail.com>
2024-09-04 19:00:57 +0100

    fix error

--- planetboundmass/boundmass.py
+++ planetboundmass/boundmass.py
@@ -325,7 +325,7 @@ class Bound:
                 )
                 sel_redis_bound = ke + pe < 0.0
 
-                self.bound[sel_redis_bound] = rem_bid
+                self.bound[self.bound == bid][sel_redis_bound] = rem_bid
 
                 if verbose:
                     print(
