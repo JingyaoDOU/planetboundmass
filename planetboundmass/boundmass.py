a319fe31c95765aae06997b3bd5e41d5f3f29da1
JingyaoDOU <jingyaodou.uk@gmail.com>
2024-09-05 18:09:54 +0100

    test

--- planetboundmass/boundmass.py
+++ planetboundmass/boundmass.py
@@ -327,16 +327,41 @@ class Bound:
         # for bid in self.bound_id:
         #     bound_cp = copy(self.bound)
         #     for rem_bid in self.bound_id[self.bound_id > bid]:
-        rem_com = np.sum(
-            self.pos[self.bound == rem_bid] * self.m[self.bound == rem_bid, np.newaxis],
-            axis=0,
-        ) / np.sum(self.m[self.bound == rem_bid])
-        rem_vel = np.sum(
-            self.vel[self.bound == rem_bid] * self.m[self.bound == rem_bid, np.newaxis],
-            axis=0,
-        ) / np.sum(self.m[self.bound == rem_bid])
-
         rem_m = np.sum(self.m[self.bound == rem_bid])
+
+        rem_com = (
+            np.sum(
+                self.pos[self.bound == rem_bid]
+                * self.m[self.bound == rem_bid, np.newaxis],
+                axis=0,
+            )
+            / rem_m
+        )
+        rem_vel = (
+            np.sum(
+                self.vel[self.bound == rem_bid]
+                * self.m[self.bound == rem_bid, np.newaxis],
+                axis=0,
+            )
+            / rem_m
+        )
+
+        c_rem_m = np.sum(self.m[self.bound == bid])
+
+        c_rem_com = (
+            np.sum(
+                self.pos[self.bound == bid] * self.m[self.bound == bid, np.newaxis],
+                axis=0,
+            )
+            / c_rem_m
+        )
+        c_rem_vel = (
+            np.sum(
+                self.vel[self.bound == bid] * self.m[self.bound == bid, np.newaxis],
+                axis=0,
+            )
+            / c_rem_m
+        )
 
         ke = (
             0.5
@@ -350,7 +375,21 @@ class Bound:
                 self.pos[self.bound == bid, 1] - rem_com[1],
             ),
         )
-        sel_redis_bound = ke + pe < 0.0
+
+        c_ke = (
+            0.5
+            * self.m[self.bound == bid]
+            * np.sum((self.vel[self.bound == bid] - c_rem_vel) ** 2, axis=1)
+        )
+        c_pe = (-Bound.G * c_rem_m * self.m[self.bound == bid]) / np.hypot(
+            self.pos[self.bound == bid, 2] - c_rem_com[2],
+            np.hypot(
+                self.pos[self.bound == bid, 0] - c_rem_com[0],
+                self.pos[self.bound == bid, 1] - c_rem_com[1],
+            ),
+        )
+
+        sel_redis_bound = (ke + pe < 0.0) & (c_pe > pe)
 
         bid_mask = self.bound == bid
         update_mask = np.zeros_like(bound_cp, dtype=bool)
