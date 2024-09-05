55a50550cce296ecfd6f1e35b0b7e2dcbbc34cfd
JingyaoDOU <jingyaodou.uk@gmail.com>
2024-09-05 18:35:16 +0100

    test

--- planetboundmass/boundmass.py
+++ planetboundmass/boundmass.py
@@ -326,7 +326,7 @@ class Bound:
         # bound_cp = copy(self.bound)
         for bid in self.bound_id[::-1]:
             bound_cp = copy(self.bound)
-            for rem_bid in self.bound_id[self.bound_id < bid]:
+            for rem_bid in self.bound_id[self.bound_id != bid]:
                 rem_m = np.sum(self.m[self.bound == rem_bid])
 
                 rem_com = (
